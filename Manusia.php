44d02ca286d6105593b626478ae91bdd4143cd20
Khoirul Umam <khoirul.umam35@gmail.com>
2016-08-26 19:22:43 +0700

    Update Manusia.php
    Menambahkan definisi polymorphism

--- Manusia.php
+++ Manusia.php
@@ -1,5 +1,7 @@
 <?php
 	class Manusia{
+		// Polymorphism: satu nama banyak bentuk
+		// Banyak bentuk bermakna satu function bisa menerima bervariasi jumlah dan macam parameter
 		function tidur($waktu = "malam"){
 			if($waktu == "malam")
 				echo "Memejamkan mata";
